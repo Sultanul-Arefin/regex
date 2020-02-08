c8b69254bb7ba16c831feba86ee714c5c5c0dcc6
Sultanul Arefin <fahimsultan4@gmail.com>
2020-02-09 03:15:08 +0600

    after completing lesson 15, email regex pattern

--- validation.js
+++ validation.js
@@ -12,7 +12,25 @@ const patterns={
 	username: /^[a-z\d]{5,12}$/i, // 5 to 12 characters long, starts with a to z or number, case insensitive
 	password: /^[\w@-]{8,20}$/, // \w=> any chars, any digit, _ too,,, @,- included, 8 to 20 characters
 	slug: /^[a-z\d-]{8,20}$/, //lowercase, digits, - could contain, 8 to 20 characters long
+	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 };
+
+/*
+(yourname) @ (domain) . (extension) (.again)
+1. any letters, numbers, dots and/or hypens
+2. any letters, numbers and/or hypens
+3. any letters
+4. a dot(.) then any letters (optional)
+*/
+
+
+/*
+email => 4 part
+1st part => ([a-z\d\.-]+) => only small letters, digits, '.', '-', as many times can be included 
+2nd part => ([a-z\d-]+)
+3rd part => ([a-z]{2,8})
+4th part => (\.[a-z]{2,8})? => this part is optional that's why used ?
+*/
 
 // validation function
 function validate(field,regex){
