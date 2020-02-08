15a7de70679d2a8f670fc4f47b311c10887a790a
Sultanul Arefin <fahimsultan4@gmail.com>
2020-02-09 00:33:31 +0600

    after completing lesson 14, username, password, slug done

--- validation.js
+++ validation.js
@@ -9,6 +9,9 @@ const inputs = document.querySelectorAll('input');
 // 11 digit phone number
 const patterns={
 	telephone: /^\d{11}$/, // ^ => 11 digit from the start, $ => 11 digit from the end
+	username: /^[a-z\d]{5,12}$/i, // 5 to 12 characters long, starts with a to z or number, case insensitive
+	password: /^[\w@-]{8,20}$/, // \w=> any chars, any digit, _ too,,, @,- included, 8 to 20 characters
+	slug: /^[a-z\d-]{8,20}$/, //lowercase, digits, - could contain, 8 to 20 characters long
 };
 
 // validation function
