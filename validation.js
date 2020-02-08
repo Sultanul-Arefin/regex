f020fbaf82bc81eea51ea600b9e0c6330e0962ec
Sultanul Arefin <fahimsultan4@gmail.com>
2020-02-08 23:51:02 +0600

    after completing lesson 12, example of number validation in javascript

--- validation.js
+++ validation.js
@@ -1,5 +1,10 @@
 // validation script here
 
-var reg=/[a-z]/ig; //1st way
+// var reg=/[a-z]/ig; //1st way
 
-var reg2=new RegExp(/[a-z]/,'i'); //2nd way
+// var reg2=new RegExp(/[a-z]/,'i'); //2nd way
+
+// 11 digit phone number
+const patterns={
+	telephone: /^\d{11}$/, // ^ => 11 digit from the start, $ => 11 digit from the end
+}
