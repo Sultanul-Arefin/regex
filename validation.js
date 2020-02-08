ccc0be4c5a807ca55314e2aa4ea4d7976ade464c
Sultanul Arefin <fahimsultan4@gmail.com>
2020-02-09 00:20:33 +0600

    after completing lesson 13, telephone validation with addClass

--- validation.js
+++ validation.js
@@ -4,7 +4,26 @@
 
 // var reg2=new RegExp(/[a-z]/,'i'); //2nd way
 
+const inputs = document.querySelectorAll('input');
+
 // 11 digit phone number
 const patterns={
 	telephone: /^\d{11}$/, // ^ => 11 digit from the start, $ => 11 digit from the end
+};
+
+// validation function
+function validate(field,regex){
+	// console.log(regex.test(field.value));
+	if(regex.test(field.value)){
+		field.className = 'valid';
+	} else {
+		field.className = 'invalid';
+	}
 }
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', (e) => {
+		// console.log(e.target.attributes.name.value);
+		validate(e.target,patterns[e.target.attributes.name.value])
+	});
+});
